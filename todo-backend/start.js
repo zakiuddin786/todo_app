794b7e45c52fd67634629b5c56400d065990e19f
Zakiuddin <zakiuddin321@gmail.com>
2025-02-25 22:24:41 +0530

    Pushing the docker images into AWS ECR

--- todo-backend/start.js
+++ todo-backend/start.js
@@ -1,6 +1,6 @@
 const app = require("./server")
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT,()=>{
     console.log(`Server is running on the port ${PORT}`)
 })
